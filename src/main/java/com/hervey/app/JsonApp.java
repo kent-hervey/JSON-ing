a56331bd5663617d560ad9471d8a92efe4253c2e
Kent Hervey <kent.hervey@employbridge.com>
2022-01-23 17:38:14 -0600

    added Staff.java and updated JsonApp.java

--- src/main/java/com/hervey/app/JsonApp.java
+++ src/main/java/com/hervey/app/JsonApp.java
@@ -3,5 +3,11 @@ package com.hervey.app;
 public class JsonApp {
     public static void main(String[] args){
         System.out.println("running2");
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/com/hervey/app/Staff.java
@@ -0,0 +1,17 @@
+package com.hervey.app;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class Staff {
+
+    private String name;
+    private int age;
+    private String[] position;              // array
+    private List<String> skills;            // list
+    private Map<String, BigDecimal> salary; // map
+
+    //getters and setters
+}
